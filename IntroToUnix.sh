0cd438bd8c6babea578fa666ba26d919f14d3875
Carrie Ganote <cganote@h2.carbonate.uits.iu.edu>
2018-10-12 16:07:15 -0400

    just in case

--- IntroToUnix.sh
+++ IntroToUnix.sh
@@ -71,12 +71,12 @@ function dirtut(){
     read -p "Press enter to continue"
     clear
 
-    ponysay -b round -F rainbowfirm $'The first thing you want to learn, is what directories come standard in Unix.\n\n If you do cd .. until you get dizzy, you will end up in the mega-parent directory. This directory is called the root directory, and it is denoted by a forward slash: /. \n\nThe root directory is like the base of a tree, and all other directories radiate out of it. Since directories are hierarchical, you can think of it like a tree with thick branches that split off into smaller and smaller ones, all the way down to the leaves.\n\nYou noticed that your location always starts with a / when you do pwd, right?'
+    ponysay -b round -F rainbowfirm $'The first thing you want to learn, is what directories come standard in Unix.\n\n If you do cd .. until you get dizzy, you will end up in the mega-parent directory. This directory is called the root directory, and it is denoted by a forward slash: "/". \n\nThe root directory is like the base of a tree, and all other directories grow out from it. Since directories are hierarchical, you can think of it like a tree with thick branches that split off into smaller and smaller ones, all the way down to the leaves.\n\nYou noticed that your location always starts with a / when you do pwd, right?'
     echo "Your current location:"
     pwd
     read -p "Press enter to continue"
     clear
-
+# add note that you don't have to be inside directory to run a command
     ponysay -b round -F rainbowsalute $'Inside the root directory, there are a few folders you should know about. One is the /bin directory, which contains commands that you might want to run, like bash itself. Bin stands for binary, which is the format that many commands are in - binary machine code which is ready to talk to the operating system directly. Binary files are hard to read by humans.\n\nAnother good directory to know is /usr/local, which has code in it customized to this computer. \n\nThe /lib and /lib64 directories contain libraries, which are software codes that are intended to be put inside other code and work in a modular way. \n\n The /tmp directory is a place you might end up putting temporary files that are generated while a command runs.\n\n/etc, /var, /proc, and /sbin are usually locked down to only admins - they contain operating system files. '
     read -p "Press enter to continue"
     clear
